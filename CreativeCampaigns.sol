dfe11f7995815bed4750da64e38c7baf5afa3b60
PJFenton1 <125835029+PJFenton1@users.noreply.github.com>
2024-04-03 14:59:59 -0400

    Update CreativeCampaigns.sol
    Updated CreativeCampaigns with more functions

--- CreativeCampaigns.sol
+++ CreativeCampaigns.sol
@@ -10,44 +10,31 @@ contract CreativeCampaigns is ERC721URIStorage, Ownable{
     // create a struct for the campaign data
     struct CampaignDatainfo {
         address campaignOwner;
-        string campaignURL;
-        uint256 totalCreatives;
+        string campaignName;
+        string campaignPrompt;
+        uint256 campaignBalance;
+        uint256 campaignExpense;
+        uint256 campaignID;
+        uint256 leads;
+        string promoCode;
+        string redirectLink;
+        bool paused;
+        uint256 activeGames;
         uint256 activeCreatives;
         uint256 totalGames;
-        uint256 activeGames;
-        bool paused;
+        uint256 totalCreatives;
+        uint256 totalHolders;
         uint256 totalViews;
-        uint256 totalLeads;
-    }
-    
-    // create a struct for the campaign data
-    struct CreativeDatainfo {
-        uint256 campaignID;
-        uint256 gameID;
-        uint256 holderID;
-        uint256 gameClientHolderID;
-        uint256 totalViews;
-        uint256 totalLeads;
-        bool paused;
-        string creativeURL;
     }
 
-    // Campaign mappings
     mapping(uint256 => CampaignDatainfo) public _campaigns;
-    mapping(uint256 => uint256) public _campaignBalances;
-    mapping(uint256 => uint256) public _campaignExpenses;
-    
-    // Creatives mappings
-    mapping(uint256 => CreativeDatainfo) public _creatives;
-    mapping(uint256 => uint256) public _creativesExpenses;
 
-    // TODO Campaigns functions
-    // TODO Creatives functions
-    // TODO check below
+    mapping(uint256 => uint256) public campaignBalance;
+    mapping(uint256 => uint256) public campaignExpense;
+    
 
     event ViewOccurred(uint256 _campaigns);
     constructor(address initialOwner) Ownable(initialOwner) ERC721("Campaign NFT", "CNFT") ERC721URIStorage() {
-        // TODO Campaign ID - start numeration from 1.
         _campaigns[1].totalViews = 1;
         
     }
@@ -113,11 +100,15 @@ contract CreativeCampaigns is ERC721URIStorage, Ownable{
         return _campaigns[_campaignID].totalHolders;
     }
 
-    function updateCampaign(uint256 _tokenId, string memory _data) external onlyOwner {
-        require(_exists(_tokenId), "Campaign with given token ID does not exist");
-        campaigns[_tokenId].data = _data;
+    function updateCampaign(uint256 _tokenId, CampaignDatainfo memory newData) external onlyOwner {
+        require(_campaigns[_tokenId].campaignOwner != address(0), "Campaign with given token ID does not exist");
+        _campaigns[_tokenId] = newData;
     }
 
+    function addCreativeViews(uint256 _campaignID, uint256 value) public returns (uint256) {
+        return _campaigns[_campaignID].totalViews += value;
+    }
+    
     function getCampaignViews(uint256 _campaignID) public view returns (uint256) {
         return _campaigns[_campaignID].totalViews;
     }
@@ -129,21 +120,30 @@ contract CreativeCampaigns is ERC721URIStorage, Ownable{
     function getRedirectLink(uint256 _campaignID) public view returns (string memory ) {
         return _campaigns[_campaignID].redirectLink;
     }
+
+    function getLeads(uint256 _campaignID) public view returns (uint256) {
+        return _campaigns[_campaignID].leads;
+    }
+    
+    function addCreativeLeads(uint256 _campaignID, uint256 value) public returns (uint256) {
+        return _campaigns[_campaignID].leads += value;
+    }
+
+    
+
     
     ERC721 public nftContract;
 
     // Event emitted when an NFT is minted
     event NFTMinted(address indexed owner, uint256 indexed tokenId, string tokenURI);
-
     event DataForTableland(uint indexed _campaignID, string _campaign);
 
+    
     // Function to mint an NFT using ID and enum logos
     function newCampaignNFT(uint256 _campaignID) public {
         // Get campaign data
         CampaignDatainfo memory campaignInfo = _campaigns[_campaignID];
 
-        
-        
         // Mint NFT
         uint256 tokenId = campaignInfo.campaignID;
         _safeMint(msg.sender, tokenId); // Minting the NFT
@@ -154,8 +154,8 @@ contract CreativeCampaigns is ERC721URIStorage, Ownable{
     }
 
     //Push or stream data to tableland database        
-    function storeData(uint _campaignID, string memory _campaignName) public {
-        emit DataForTableland(_campaignID, _campaignName);
+        function storeData(uint _campaignID, string memory _campaignName) public {
+            emit DataForTableland(_campaignID, _campaignName);
     }
         
     function viewTopUpBallance() public view returns(uint256) {
@@ -165,5 +165,8 @@ contract CreativeCampaigns is ERC721URIStorage, Ownable{
     function withdrawBalance() public payable onlyOwner {
         uint256 balance = address(this).balance;
         payable(owner()).transfer(balance);
-    }    
+    }
+
+    
+    
 }
