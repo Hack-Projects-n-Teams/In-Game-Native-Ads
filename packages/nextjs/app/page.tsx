0c9ed18be5e59a0d1b1ffd77e9aeafe4fd606975
AlienX <aditya1115.ar@gmail.com>
2024-04-03 20:40:03 +0530

    Your commit message

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -3,7 +3,6 @@
 import Link from "next/link";
 import type { NextPage } from "next";
 import { useAccount } from "wagmi";
-import { NewListing } from "~~/components/listing";
 // import { BugAntIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import { Address } from "~~/components/scaffold-eth";
 
